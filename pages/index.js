cf272050197ca1199618de6e35857844f77257de
therandomfool <mddenver@protonmail.com>
2021-06-20 17:45:32 -0600

    added CSS

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
-import Head from 'next/head'
+import { useState } from 'react'
 import CoinList from '../components/CoinList'
 import SearchBar from '../components/SearchBar'
 import Layout from '../components/Layout'
 
 
 export default function Home({ filteredCoins }) {
-  console.log(filteredCoins)
+  const [search, setSearch] = useState('')
+
+  const allCoins = filteredCoins.filter(coin => 
+    coin.name.toLowerCase().includes(search.toLowerCase()))
+
+  const handleChange = e => {
+    e.preventDefault()
+
+    setSearch(e.target.value.toLowerCase())
+  }
   return (
     <Layout >
       <div className="coin_app">
-        <SearchBar type="text" placeholder="Search" />
-        <CoinList filteredCoins={filteredCoins} />
+        <SearchBar type="text" placeholder="Search" 
+        onChange={handleCHange}/>
+        <CoinList filteredCoins={allCoins} />
       </div>
 
 
